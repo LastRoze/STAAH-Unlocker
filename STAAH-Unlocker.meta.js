db469e2ddcf1b1d4617485d5c70370a350244934
Yoga <LastRoze@users.noreply.github.com>
2019-11-09 03:20:42 +0700

    Create STAAH-Unlocker.meta.js

---
+++ STAAH-Unlocker.meta.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         STAAH Unlocker
+// @namespace    STAAH Unlocker
+// @description  ZUZU Hospitality - STAAH Unlocker is created to unlock 2 hidden feature from STAAH mapping system.
+// @copyright    ©2019 - Yoga Budiman
+// @version      1.0
+// @author       Last Roze
+// @homepageURL  https://github.com/LastRoze/STAAH-Unlocker
+// @supportURL   https://linkedin.com/in/lastroze
+// @updateURL    https://github.com/LastRoze/STAAH-Publisher/raw/master/STAAH-Unlocker.meta.js
+// @downloadURL  https://github.com/LastRoze/STAAH-Publisher/raw/master/STAAH-Unlocker.user.js
+// @icon         https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco/v1503462675/pnd8ig9snambqtixjnge.jpg
+// @grant        GM_addStyle
+// @grant        GM_deleteValue
+// @grant        GM_getResourceURL
+// @grant        GM_getValue
+// @grant        GM_openInTab
+// @grant        GM_registerMenuCommand
+// @grant        GM_setValue
+// @grant        GM_xmlhttpRequest
+// @grant        GM.deleteValue
+// @grant        GM.getResourceUrl
+// @grant        GM.getValue
+// @grant        GM.openInTab
+// @grant        GM.xmlHttpRequest
+// @grant        unsafeWindow
+// @run-at       document-start
+// @match        *://cp.staah.net/*
+// @noframes
+// ==/UserScript==
